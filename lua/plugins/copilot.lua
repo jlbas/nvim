7da636d601260b1ad3de08f88864348703e9caad
Jean-Luc Bastarache <jeanluc.bastarache@gmail.com>
2023-12-16 09:30:50 -0500

    Add lazy copilot condition to enable on `uname`

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,6 +2,19 @@ return {
   {
     'zbirenbaum/copilot.lua',
     build = ':Copilot auth',
+    cond = function()
+      local whoami_match = 'jbastara'
+      local uname_match = 'T495'
+      local whoami = string.sub(
+        vim.system({ 'whoami' }, { text = true }):wait().stdout,
+        1, string.len(whoami_match)
+      )
+      local uname = string.sub(
+        vim.system({ 'uname', '-n' }, { text = true }):wait().stdout,
+        1, string.len(uname_match)
+      )
+      return whoami == whoami_match and uname == uname_match
+    end,
     cmd = 'Copilot',
     event = 'InsertEnter',
     opts = {},
