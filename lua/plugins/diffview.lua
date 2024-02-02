5eda869b7f908ddb27f133fb527b9d76455f80be
Jean-Luc Bastarache <jeanluc.bastarache@gmail.com>
2024-02-02 11:49:29 -0500

    Quit when diffview closes and only the initial empty buf remains

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -4,6 +4,13 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
       enhanced_diff_hl = true,
+      hooks = {
+        view_closed = function()
+          if vim.fn.bufname('%') == '' and vim.fn.bufnr('%') == 1 then
+            vim.cmd('quit')
+          end
+        end,
+      }
     },
   },
 }
