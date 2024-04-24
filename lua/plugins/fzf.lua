215eaad452858f0f3b4293b65893c4823ab8fd1d
bastarac <sr-build@list.nokia.com>
2024-04-24 15:58:02 -0400

    Modify fzf-lua keybindings

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -7,6 +7,29 @@ return {
         fzf_opts = {
           ['--cycle'] = '',
         },
+        args = { actions = { ["ctrl-x"] = false } },
+        buffers = { actions = { ["ctrl-x"] = false } },
+        git = {
+          stash = { actions = { ["ctrl-x"] = false } },
+          branches = { actions = { ["ctrl-x"] = false } },
+          status = { actions = { ["ctrl-x"] = false } },
+        },
+        keymap = {
+          builtin = {
+            ["<C-d>"] = "preview-page-down",
+            ["<C-u>"] = "preview-page-up",
+          },
+          fzf = {
+            ["ctrl-d"] = "preview-page-down",
+            ["ctrl-u"] = "preview-page-up",
+          },
+        },
+        previewers = {
+          builtin = {
+            syntax_limit_b = 2048*2048,
+          },
+        },
+        tabs = { actions = { ["ctrl-x"] = false } },
         winopts = {
           border = 'single',
         },
