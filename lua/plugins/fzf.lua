766c3a50c723830c7d303905a1f2779625068fb3
bastarac <jean-luc.bastarache@nokia.com>
2024-06-20 09:37:07 -0400

    Add pick window custom actions to fzf-lua using nvim-window

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,21 +3,39 @@ return {
     'ibhagwan/fzf-lua',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+      local actions = require('fzf-lua.actions')
+
+      function actions.file_pick_window(selected, opts)
+        require('nvim-window').pick()
+        actions.file_edit(selected, opts)
+      end
+
+      function actions.buf_pick_window(selected, opts)
+        require('nvim-window').pick()
+        actions.buf_edit(selected, opts)
+      end
+
       require("fzf-lua").setup({
         fzf_opts = {
           ['--cycle'] = '',
         },
         args = { actions = { ["ctrl-x"] = false } },
-        buffers = { actions = { ["ctrl-x"] = false } },
+        buffers = {
+          actions = {
+            ["ctrl-x"] = false,
+            ["ctrl-o"] = actions.buf_pick_window,
+          },
+        },
         git = {
           stash = { actions = { ["ctrl-x"] = false } },
           branches = { actions = { ["ctrl-x"] = false } },
           status = { actions = { ["ctrl-x"] = false } },
         },
+        files = { actions = { ["ctrl-o"] = actions.file_pick_window } },
         keymap = {
           builtin = {
-            ["<C-d>"] = "preview-page-down",
-            ["<C-u>"] = "preview-page-up",
+            ["ctrl-d"] = "preview-page-down",
+            ["ctrl-u"] = "preview-page-up",
           },
           fzf = {
             ["ctrl-d"] = "preview-page-down",
---
+++ lua/plugins/window.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'https://gitlab.com/yorickpeterse/nvim-window.git',
+    opts = {
+      normal_hl = 'Normal',
+      hint_hl = 'Bold',
+      border = 'single',
+    },
+  },
+}
