67747c7bc2a84a394d004e27377a99a8c27071f2
bastarac <jean-luc.bastarache@nokia.com>
2024-07-25 08:15:13 -0400

    Merge fzf buf/file pick-window function into one

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,30 +3,29 @@ return {
     'ibhagwan/fzf-lua',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      local actions = require('fzf-lua.actions')
-      local default_actions = require('fzf-lua').defaults.actions
+      local fzf_lua = require('fzf-lua')
+      local actions = fzf_lua.actions
+      local default_actions = fzf_lua.defaults.actions
       local nvim_window = require('nvim-window')
 
-      function actions.file_pick_window(selected, opts)
-        nvim_window.pick()
-        actions.file_edit(selected, opts)
-      end
-
-      function actions.buf_pick_window(selected, opts)
+      function actions.pick_window(selected, opts)
         local last_win = vim.fn.win_getid()
         nvim_window.pick()
         if vim.fn.win_getid() ~= last_win then
-          actions.buf_edit(selected, opts)
+          if selected[1]:sub(1,1) == '[' then
+            actions.buf_edit(selected, opts)
+          else
+            actions.file_edit(selected, opts)
+          end
         end
       end
 
-      default_actions.buffers["ctrl-]"] = actions.buf_pick_window
-      default_actions.files["ctrl-]"] = actions.file_pick_window
+      default_actions.buffers["ctrl-q"] = actions.pick_window
+      default_actions.files["ctrl-q"] = actions.pick_window
 
-      require("fzf-lua").setup({
+      fzf_lua.setup({
         fzf_opts = { ['--cycle'] = '' },
         args = { actions = { ["ctrl-x"] = false } },
-        buffers = { actions = { ["ctrl-x"] = false } },
         git = {
           stash = { actions = { ["ctrl-x"] = false } },
           branches = { actions = { ["ctrl-x"] = false } },
