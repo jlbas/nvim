4bd287c37cbb233beec3b82d9ddc9297c74232bf
bastarac <jean-luc.bastarache@nokia.com>
2024-12-19 13:53:44 -0500

    Improve modus highlights

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -95,9 +95,13 @@ return {
         highlight.LineNr         = { fg = color.fg_dim, bg = nil }
         highlight.LineNrAbove    = { fg = color.fg_dim, bg = nil }
         highlight.LineNrBelow    = { fg = color.fg_dim, bg = nil }
-        -- highlight.GitSignsAdd    = { fg = color.fg_added_intense, bg = nil }
-        -- highlight.GitSignsChange = { fg = color.fg_changed_intense, bg = nil }
-        -- highlight.GitSignsDelete = { fg = color.fg_removed_intense, bg = nil }
+        highlight.DiffAdd        = { bg = color.bg_added_faint }
+        highlight.DiffChange     = { bg = color.bg_changed_faint }
+        highlight.DiffDelete     = { bg = color.bg_removed_faint }
+        highlight.DiffText       = { bg = color.bg_changed_faint }
+        highlight.GitSignsAdd    = { fg = color.fg_added, bg = nil }
+        highlight.GitSignsChange = { fg = color.fg_changed, bg = nil }
+        highlight.GitSignsDelete = { fg = color.fg_removed, bg = nil }
       end,
     },
     priority = 1000,
