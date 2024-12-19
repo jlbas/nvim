716db056d79b7c9927d2bc30be88bd5078e89b41
bastarac <jean-luc.bastarache@nokia.com>
2024-12-19 14:35:38 -0500

    Switch to `neodev`

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -2,7 +2,18 @@ return {
   {
     'saghen/blink.cmp',
     lazy = false,
-    dependencies = 'rafamadriz/friendly-snippets',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      }
+    },
     version = 'v0.*',
     opts = {
       keymap = {
@@ -12,7 +23,11 @@ return {
         use_nvim_cmp_as_default = true,
       },
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
+      },
+      providers = {
+        lsp = { fallback_for = { 'lazydev' } },
+        lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink' },
       },
       signature = { enabled = true }
     },
