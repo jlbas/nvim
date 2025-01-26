0072e7152ed95368c91a0abb2e7b07ab2647ab8e
bastarac <jean-luc.bastarache@nokia.com>
2025-01-26 08:39:51 -0500

    Disable session autocmd when vim is opened with `+Man!`

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -16,7 +16,13 @@ vim.api.nvim_create_autocmd('FileType', {
 
 vim.api.nvim_create_autocmd({ 'VimEnter' }, {
   callback = function()
-    local base_dir = vim.system({ 'git', 'rev-parse', '--show-toplevel' }, { text = true }):wait()
+    for _, arg in ipairs(vim.v.argv) do
+      if arg == '+Man!' then
+        return
+      end
+    end
+
+    local base_dir = vim.system({ 'git', 'remote', 'get-url', 'origin' }, { text = true }):wait()
     if base_dir.code ~= 128 and base_dir.stdout:find('panos') then
       local branch = vim.system({ 'git', 'branch', '--show-current' }):wait().stdout:gsub('\n', '')
       local file = vim.fn.expand('~/.local/state/nvim/session/' .. branch .. '.vim')
