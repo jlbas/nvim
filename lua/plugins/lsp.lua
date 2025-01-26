b62fb069e9cbbd4a9aba46df060467591752ff08
bastarac <jean-luc.bastarache@nokia.com>
2025-01-26 08:47:23 -0500

    Specify border chars for lsp floating windows

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -109,6 +109,24 @@ return {
           end, opts)
         end,
       })
+
+      local border = {
+        { '┌', 'FloatBorder' },
+        { '─', 'FloatBorder' },
+        { '┐', 'FloatBorder' },
+        { '│', 'FloatBorder' },
+        { '┘', 'FloatBorder' },
+        { '─', 'FloatBorder' },
+        { '└', 'FloatBorder' },
+        { '│', 'FloatBorder' },
+      }
+
+      local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+      function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+        opts = opts or {}
+        opts.border = opts.border or border
+        return orig_util_open_floating_preview(contents, syntax, opts, ...)
+      end
     end,
   },
   {
