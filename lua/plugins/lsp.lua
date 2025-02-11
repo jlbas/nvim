cb9634909e2878352a1cd859b10cd497f488cbee
bastarac <jean-luc.bastarache@nokia.com>
2025-02-11 10:35:22 -0500

    Make use of the new `statuscolumn %l` item

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -77,16 +77,22 @@ return {
       vim.keymap.set('n', '<leader>lq', vim.diagnostic.setloclist)
 
       vim.diagnostic.config({
-        signs = false,
-        underline = false,
-        virtual_text = {
-          format = function()
-            return ""
-          end,
-          prefix = "● ",
-          spacing = 1,
-          virt_text_pos = "right_align",
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = '',
+            [vim.diagnostic.severity.WARN] = '',
+            [vim.diagnostic.severity.INFO] = '',
+            [vim.diagnostic.severity.HINT] = '',
+          },
+          numhl = {
+            [vim.diagnostic.severity.ERROR] = 'DiagnosticError',
+            [vim.diagnostic.severity.WARN] = 'DiagnosticWarn',
+            [vim.diagnostic.severity.INFO] = 'DiagnosticInfo',
+            [vim.diagnostic.severity.HINT] = 'DiagnosticHint',
+          }
         },
+        underline = false,
+        virtual_text = false,
       })
 
       vim.api.nvim_create_autocmd('LspAttach', {
