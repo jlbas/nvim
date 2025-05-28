03c18c8c44b84957db9f4d546a11ac9a519d7bdc
bastarac <jean-luc.bastarache@nokia.com>
2025-05-28 14:45:43 -0400

    Fix deprecation warning

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -8,14 +8,13 @@ return {
       vim.fn.sign_define('DapLogPoint', { text = '', texthl = 'DapBreakpoint' })
       vim.fn.sign_define('DapStopped', { text = '', texthl = 'DapStoppedText', linehl = 'DapStoppedLine' })
       vim.fn.sign_define('DapBreakpointRejected', { text = '', texthl = 'DapBreakpoint' })
-      local debugpy_path = require("mason-registry").get_package("debugpy"):get_install_path()
       require('dap').configurations.python = {
         {
           type = 'python',
           name = 'Launch file',
           request = 'launch',
           program = '${file}',
-          pythonPath = debugpy_path .. "/venv/bin/python",
+          pythonPath = vim.fn.exepath("debugpy") .. "/venv/bin/python",
           args = { "kan2privvm84", "A", "1", "demo_req.json" },
         },
       }
@@ -23,8 +22,7 @@ return {
     dependencies = {
       "mfussenegger/nvim-dap-python",
       config = function()
-        local path = require("mason-registry").get_package("debugpy"):get_install_path()
-        require("dap-python").setup(path .. "/venv/bin/python")
+        require("dap-python").setup(vim.fn.exepath("debugpy") .. "/venv/bin/python")
       end,
     },
   },
