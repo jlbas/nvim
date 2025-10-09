4d948c9c654f67a1be262d5c6c5f937d48b72c73
Jean-Luc Bastarache <jeanluc.bastarache@gmail.com>
2025-10-09 13:34:44 -0400

    Specify blink.cmp version to download binary rather than build

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,6 +1,9 @@
 vim.pack.add({
   'https://github.com/rafamadriz/friendly-snippets',
-  'https://github.com/saghen/blink.cmp',
+  {
+    src = 'https://github.com/saghen/blink.cmp',
+    version = vim.version.range('1.*'),
+  },
 })
 
 require('blink.cmp').setup({
