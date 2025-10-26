26a40686178229e5426acfc0ffe14e2296fb457d
bastarac <jean-luc.bastarache@nokia.com>
2025-10-26 07:54:21 -0400

    Remove markview.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,4 @@
 vim.pack.add({
-  'https://github.com/OXY2DEV/markview.nvim',
   'https://github.com/godlygeek/tabular',
   'https://github.com/tpope/vim-fugitive',
   'https://github.com/tpope/vim-sleuth',
