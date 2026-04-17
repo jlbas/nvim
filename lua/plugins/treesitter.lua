bf7d83d4e94ddefa64b77cac434a5fa63cfec284
Jean-Luc Bastarache <jeanluc.bastarache@gmail.com>
2026-04-17 19:00:30 -0400

    Fix treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,4 @@
-vim.pack.add({'https://github.com/nvim-treesitter/nvim-treesitter'})
-
-require('nvim-treesitter.configs').setup({
-	auto_install = true,
-	highlight = { enable = true },
-	indent = { enable = true, disable = { 'c', 'cpp' } },
-})
+vim.pack.add({{
+	src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+	version = "main",
+}})
