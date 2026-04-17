b4bc2f697f0414ddd937e4653dc649d1ed4a6fbe
Jean-Luc Bastarache <jeanluc.bastarache@gmail.com>
2026-04-17 19:34:15 -0400

    Add treesitter parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,3 +2,40 @@ vim.pack.add({{
 	src = 'https://github.com/nvim-treesitter/nvim-treesitter',
 	version = "main",
 }})
+
+local parsers = {
+	'arduino',
+	'bash',
+	'c',
+	'cmake',
+	'cmake',
+	'cpp',
+	'csv',
+	'dockerfile',
+	'editorconfig',
+	'json',
+	'lua',
+	'markdown',
+	'markdown_inline',
+	'python',
+	'rust',
+	'toml',
+	'vim',
+	'vimdoc',
+	'yaml',
+}
+
+require('nvim-treesitter').install(parsers)
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = parsers,
+	callback = function()
+		-- syntax highlighting, provided by Neovim
+		vim.treesitter.start()
+		-- folds, provided by Neovim
+		vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+		vim.wo.foldmethod = 'expr'
+		-- indentation, provided by nvim-treesitter
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+	end,
+})
